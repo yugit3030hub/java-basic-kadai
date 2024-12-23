402342bfa20a69ba09e3b0e097ab8ee24078e118
yugit3030hub <yuua2002357159123@gmail.com>
2024-12-23 12:57:48 +0900

    kadai_004を追加しました

---
+++ kadai_004/__Score_Chapter04.java
@@ -0,0 +1,38 @@
+package kadai_004;
+
+public class __Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		int heikin = 0;
+		
+		System.out.println("Aさんの点数は10点");
+		
+		System.out.println("Bさんの点数は20点");
+		
+		System.out.println("Cさんの点数は30点");
+		
+		System.out.println("Dさんの点数は40点");
+		
+		System.out.println("Eさんの点数は50点");
+		
+		System.out.println("Fさんの点数は60点");
+		
+		System.out.println("Gさんの点数は70点");
+		
+		System.out.println("Hさんの点数は80点");
+		
+		System.out.println("Iさんの点数は90点");
+		
+		System.out.println("Jさんの点数は100点");
+		
+		heikin = (10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100) /10;
+		
+		
+		System.out.println("テストの平均点は" + heikin);
+		
+
+	}
+
+}
