4e5e353f48c9ebd0cf4a36c097c680d3b5e0476b
yugit3030hub <yuua2002357159123@gmail.com>
2025-01-04 16:11:51 +0900

    kadai_014をアップロードしました

---
+++ kadai_014/__CarExec_Chapter14.java
@@ -0,0 +1,12 @@
+package kadai_014;
+
+public class __CarExec_Chapter14 {
+	public static void main(String[] args) {
+	__Car_chapter14 gear = new __Car_chapter14(1,10);
+	
+	gear.gearChange(3);
+	gear.run(3);
+	
+
+}
+}
